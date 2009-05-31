1a2819f8118acfdd020925515b5bffd94f6a2654
Fate <fate-tmw@googlemail.com>
2009-05-31 13:24:25 +0000

    Fixed build-magic to only require a single sed process

--- conf/build-magic.sh
+++ conf/build-magic.sh
@@ -1,21 +1,21 @@
-#! /bin/sh
+#! /bin/bash
 
-RSCRIPT="spells-build.sh"
+RSCRIPT="spells-build"
 
 if [ -a $RSCRIPT ]; then printf ""; else
-    echo '#! /bin/sh' > $RSCRIPT;
-    echo 'cat \' >> $RSCRIPT;
+    echo '#! /bin/bash' > $RSCRIPT;
+    echo 'sed \' >> $RSCRIPT;
     chmod a+x $RSCRIPT
 fi
 
 for n in `grep -o '"#..."' magic.conf.template`; do
     if grep $n $RSCRIPT; then printf ""; else
         CHANGES=1
-        echo "|sed 's/${n}/${n}/' \\" >> $RSCRIPT;
+        echo "'s/${n}/${n}/;'\\" >> $RSCRIPT;
     fi
 done
 
 if [ x$CHANGES == x1 ]
-then echo "spells-build.sh has been updated; please provide invocations for spells and/or teleport anchors.";
+then echo "${RSCRIPT} has been updated; please provide invocations for spells and/or teleport anchors.";
 else cat magic.conf.template | ./$RSCRIPT > magic.conf;
 fi
